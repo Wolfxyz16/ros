a3daf2d054ff02ad8116940be771bf5e694a6399
Wolfxyz16 <yerayliloaiza@gmail.com>
2024-11-15 18:27:20 +0100

    first commit

--- build/wall_following/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/users/956024/ir/ros/install/wall_following'
--- build/wall_following/share/wall_following/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/users/956024/ir/ros/build/wall_following"
